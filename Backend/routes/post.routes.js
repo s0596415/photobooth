d98eb944c259ea5f3ec91514a083c9fd359b276c
Laura <laura.anter@student.htw-berlin.de>
2026-09-12 16:12:36 +0200

    Add all CRUD variations

--- Backend/routes/post.routes.js
+++ Backend/routes/post.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { collection } = require('../configure/db')
+const  ObjectId = require('mongodb').ObjectId
 
 // GET all posts
 router.get('/', async(req, res) => {
@@ -9,11 +10,9 @@ router.get('/', async(req, res) => {
     res.send(allPosts);
 });
 
-module.exports = router;
-
 // POST one new post
 router.post('/', async(req, res) => {
-
+    
     try {
         const newPost = {
             title: req.body.title,
@@ -30,3 +29,67 @@ router.post('/', async(req, res) => {
         });
     }
 });
+
+// GET one post by id
+router.get('/:id', async(req, res) => {
+    
+    try {
+        const id_obj = new ObjectId(req.params.id);
+        const post = await collection.find( {_id: id_obj } ).toArray();
+        console.log('post', req.params.id)
+        res.status(202);
+        res.send(post);
+    } catch {
+        res.status(404);
+        res.send({
+            error: "Post does not exist!"
+        });
+    }
+});
+
+// PATCH (update) one post
+router.patch('/:id', async(req, res) => {
+    try {
+        const id_obj = new ObjectId(req.params.id);
+        const post = await collection.findOne({ _id: id_obj })
+
+        if (req.body.title) {
+            post.title = req.body.title
+        }
+
+        if (req.body.location) {
+            post.location = req.body.location
+        }
+
+        if (req.body.image_id) {
+            post.image_id = req.body.image_id
+        }
+
+        await collection.updateOne({ _id: id_obj }, { $set: post });
+        res.send(post)
+    } catch {
+        res.status(404)
+        res.send({ error: "Post does not exist!" })
+    }
+});
+
+// DELETE one post via id
+router.delete('/:id', async(req, res) => {
+    try {
+        const id_obj = new ObjectId(req.params.id);
+        const post = await collection.deleteOne({ _id: id_obj })
+        console.log('post', post)
+        if(post.deletedCount === 1) {
+            res.status(204)
+            res.send( { message: "deleted" })
+        } else {
+            res.status(404)
+            res.send({ error: "Post does not exist!" })
+        }
+    } catch {
+        res.status(404)
+        res.send({ error: "something wrong" })
+    }
+});
+
+module.exports = router;
