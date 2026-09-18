de556ffa15076205b421521e506d361c76d9c639
Beyza Bektasoglu <beyza.bektasoglu@student.htw-berlin.de>
2026-09-18 10:55:13 +0200

    change Hash Password for gast

--- photobooth-final-1version/script-final1.js
+++ photobooth-final-1version/script-final1.js
@@ -712,7 +712,7 @@ function downloadLogsCSV() {
 
 // Unsere Hashes
 const ADMIN_HASH = '$2b$10$r/GjfuJv4Vk/NSH9LYXIJ.T0anlWDaQ8vYvzF2NO1l7nfaDPtNPPO';
-const GAST_HASH = '$2b$10$0bPjzvfJBNDedUkdrb/.auj4yNLEXdlXgrN23aYFXT8xgiYzlcP3W';
+const GAST_HASH = '$2b$10$6GXt.RnGhffWYW5iC0yCNeI2SxXL2Ybf.jkTAqLxHpvFOVKNB0zzm';
 
 let isGast = false; //new
 
